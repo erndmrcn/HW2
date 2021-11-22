a017fedbb4b8f99ec7a65b89d516ba85cbfb5392
erndmrcn <erndmrcn42@gmail.com>
2021-11-22 21:18:49 +0300

    Utils
    Utilities are written for 8-Puzzle.
    TODO: Try to merge the utils for both 8-Puzzle and maze problems

--- hw2.py
+++ hw2.py
@@ -5,39 +5,119 @@
 # Explain the heuristic function and the reasoning behind it
 
 # global variables, data structures
+startState = []
+targetState = []
 
-def moveLeft():
+####### 8-Puzzle Utilities Start #######
+def moveLeft8(currentState):
     # "LEFT"
+    x, y = findIndex(currentState, 0)
+    if x > 0:
+        temp = currentState[x][y - 1]
+        currentState[x][y - 1] = 0
+        currentState[x][y] = temp
+
     return
 
 
-def moveUp():
+def moveUp8(currentState):
     # "UP"
+    x, y = findIndex(currentState, 0)
+    if y > 0:
+        temp = currentState[x - 1][y]
+        currentState[x - 1][y] = 0
+        currentState[x][y] = temp
+
     return
 
 
-def moveRight():
+def moveRight8(currentState):
     # "RIGHT"
+    x, y = findIndex(currentState, 0)
+    if x < 2:
+        temp = currentState[x][y + 1]
+        currentState[x][y + 1] = 0
+        currentState[x][y] = temp
+
     return
 
 
-def moveDown():
+def moveDown8(currentState):
     # "DOWN"
+    x, y = findIndex(currentState, 0)
+    if y < 2:
+        temp = currentState[x + 1][y]
+        currentState[x + 1][y] = 0
+        currentState[x][y] = temp
+
     return
 
 
 # heuristic function
 def manhattanDistance():
-    return
 
+    heuristicEval = 0
+
+    for i in range(0, 8):
+        x1, y1 = findIndex(startState, i)
+        x2, y2 = findIndex(targetState, i)
+
+        distance = abs(x2 - x1) + abs(y2- y1)
+        heuristicEval += distance
+
+    return heuristicEval
+
+
+def findIndex(arr, val):
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr[i])):
+            if arr[i][j] == val:
+                return i, j
+
+
+def convertState(state):
+    
+    res = ""
+    for row in state:
+        for col in row:
+            if col == 0:
+                res += ' '
+            else:
+                res += str(col)
+    
+    return res
 
 # read file into the variables and structures
 def readProblem(fileName):
+
+    lines = []
+
+    with open(fileName, 'r') as f:
+        lines.append(f.readline()[:-1])
+        lines.append(f.readline()[:-1])
+        lines.append(f.readline()[:-1])
+        f.readline()
+        lines.append(f.readline()[:-1])
+        lines.append(f.readline()[:-1])
+        lines.append(f.readline())
+
+    for line in lines:
+        if len(startState) < 3:
+            result = line.split(' ')
+            row = [int(result[0]), int(result[1]), int(result[2])]
+            startState.append(row)
+        elif len(targetState) < 3:
+            result = line.split(' ')
+            row = [int(result[0]), int(result[1]), int(result[2])]
+            targetState.append(row)
+
     return
 
 
 # clear global variables and data structures
 def clear():
+    startState = []
+    targetState = []
     return
 
 def InformedSearch(methodName, fileName):
@@ -68,3 +148,12 @@ def UCS():
 # A* search
 def AStar():
     return
+
+def printArr(state):
+    for row in state:
+        print(row)
+    
+    return
+
+readProblem("eightpuzzle1.txt")
+print(manhattanDistance())
